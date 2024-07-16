2978e7f50a1fbf03de347c1b596c89d30e3b9a59
Shubham Joshi <shubham@betalectic.com>
2024-07-16 15:16:50 +0530

    ping endpoint and other config added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ping', function () {
+    // console.log("Dummy change, changed session driver to cookie in env");
+    return response('SERVICE_IS_HEALTHY', 200);
+});
+
